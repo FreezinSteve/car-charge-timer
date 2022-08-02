c3f8bf6e01642a059d64d04caa63b02326d25b61
Steve de Lima <steve.delima@niwa.co.nz>
2022-08-02 21:09:35 +1200

    Saved custom settings in comments for TFT_eSPI

--- CarChargeTimer.ino
+++ CarChargeTimer.ino
@@ -25,14 +25,27 @@ const uint16_t wifi_icon[324] = {
 #include <TFT_eSPI.h>
 #include <SPI.h>
 
-// For the breakout, you can use any 2 or 3 pins
-// These pins will also work for the 1.8" TFT shield
-//#define TFT_CS     D1
-//#define TFT_RST    D0  // you can also connect this to the Arduino reset
-// in which case, set this #define pin to -1!
-//#define TFT_DC     D2
-
 TFT_eSPI tft = TFT_eSPI();  // Invoke library, pins defined in User_Setup.h
+// User_Setup.h sketch/hardware specific settings
+//  #define ST7735_DRIVER      // Define additional parameters below for this display
+//  #define TFT_RGB_ORDER TFT_BGR  // Colour order Blue-Green-Red
+//  #define TFT_WIDTH  128
+//  #define TFT_HEIGHT 128
+//  #define ST7735_GREENTAB2
+//  #define TFT_CS   PIN_D1  // Chip select control pin D8
+//  #define TFT_DC   PIN_D2  // Data Command control pin
+//  #define TFT_RST  PIN_D0  // Reset pin (could connect to NodeMCU RST, see next line)
+//  #define LOAD_GLCD   // Font 1. Original Adafruit 8 pixel font needs ~1820 bytes in FLASH
+//  #define LOAD_FONT2  // Font 2. Small 16 pixel high font, needs ~3534 bytes in FLASH, 96 characters
+//  #define LOAD_FONT4  // Font 4. Medium 26 pixel high font, needs ~5848 bytes in FLASH, 96 characters
+//  #define LOAD_FONT6  // Font 6. Large 48 pixel font, needs ~2666 bytes in FLASH, only characters 1234567890:-.apm
+//  #define LOAD_FONT7  // Font 7. 7 segment 48 pixel font, needs ~2438 bytes in FLASH, only characters 1234567890:-.
+//  #define LOAD_FONT8  // Font 8. Large 75 pixel font needs ~3256 bytes in FLASH, only characters 1234567890:-.
+//  #define LOAD_GFXFF  // FreeFonts. Include access to the 48 Adafruit_GFX free fonts FF1 to FF48 and custom fonts
+//  #define SMOOTH_FONT
+//  #define SPI_FREQUENCY  27000000
+//  #define SPI_READ_FREQUENCY  20000000
+//  #define SPI_TOUCH_FREQUENCY  25000
 //=========================================================================
 // WiFi
 #include <ESP8266WiFi.h>
